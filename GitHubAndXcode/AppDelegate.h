0a664647ee7802f483e8c79381d27ef0b73228c2
MR. BISHAL DANGOL <bsldngl@yahoo.com>
2016-05-06 17:15:17 +0545

    Initial Commit

---
+++ GitHubAndXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubAndXcode
+//
+//  Created by NITV iOS Developer on 5/6/16.
+//  Copyright © 2016 NITV iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubAndXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubAndXcode
+//
+//  Created by NITV iOS Developer on 5/6/16.
+//  Copyright © 2016 NITV iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
